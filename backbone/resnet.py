fe8f2272fdf2877392c4707c32d3d982a6b33979
hungnt0104 <111101468+hungnt0104@users.noreply.github.com>
2024-09-09 09:30:00 +0700

    change num_classes to 100

--- backbone/resnet.py
+++ backbone/resnet.py
@@ -67,7 +67,7 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=100):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
@@ -108,17 +108,17 @@ def ResNet18(num_class=100):
     return ResNet(BasicBlock, [2,2,2,2], num_classes=num_class)
 
 
-def ResNet34(num_class=10):
+def ResNet34(num_class=100):
     return ResNet(BasicBlock, [3,4,6,3], num_classes=num_class)
 
 
-def ResNet50(num_class=10):
+def ResNet50(num_class=100):
     return ResNet(Bottleneck, [3,4,6,3], num_classes=num_class)
 
 
-def ResNet101(num_class=10):
+def ResNet101(num_class=100):
     return ResNet(Bottleneck, [3,4,23,3], num_classes=num_class)
 
 
-def ResNet152(num_class=10):
+def ResNet152(num_class=100):
     return ResNet(Bottleneck, [3,8,36,3], num_classes=num_class)
